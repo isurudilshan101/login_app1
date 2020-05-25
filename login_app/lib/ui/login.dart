15044b83b7806d9d57dedee19aa753e519752cc2
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-05-25 12:11:54 +0530

    flutter practice 3

--- login_app/lib/ui/login.dart
+++ login_app/lib/ui/login.dart
@@ -52,61 +52,72 @@ class LoginState extends State<Login>{
         color: Colors.brown,
         alignment: Alignment.center,
        
-        child: Column(
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     Text("First Item", textDirection: TextDirection.ltr,
+        //     style:TextStyle(color: Colors.white
+        //     ),
+        //     ),
+
+        //     Text("Second Item", textDirection: TextDirection.ltr,
+        //     style: TextStyle(color: Colors.cyan
+        //     ),
+        //     ),
+
+        //     Container(
+        //       color:Colors.white,
+        //       alignment:Alignment.bottomLeft,
+
+        //       child: Text("third item", textDirection: TextDirection.ltr,
+        //       style: TextStyle(color: Colors.amber
+        //       ),
+        //       ),
+
+        //     ),
+
+        //     Text("fourt item",
+        //     textDirection: TextDirection.rtl,
+        //     style: TextStyle(color: Colors.blue
+        //     ),
+        //     ),
+
+        //     Container(
+        //       color: Colors.black54,
+
+        //       child: Text("fifth item", 
+        //       textDirection: TextDirection.ltr,
+        //       style:TextStyle(color: Colors.pink
+        //       ),
+        //       ),
+
+        //     ),
+        //     Text("six item",
+        //     textDirection: TextDirection.rtl,
+        //     style: TextStyle(color: Colors.blue
+        //     ),
+        //     ),
+        //   ],
+  
+
+        // ),
+      child:Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("First Item", textDirection: TextDirection.ltr,
-            style:TextStyle(color: Colors.white
-            ),
-            ),
+            Text("item 1",textDirection: TextDirection.ltr,
+            style:TextStyle(color:Colors.blue),),
 
-            Text("Second Item", textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.cyan
-            ),
-            ),
+             Text("item 2",textDirection: TextDirection.ltr,
+            style:TextStyle(color:Colors.blue),),
 
-            Container(
-              color:Colors.white,
-              alignment:Alignment.bottomLeft,
+             Text("item 3",textDirection: TextDirection.ltr,
+            style:TextStyle(color:Colors.blue),),
 
-              child: Text("third item", textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.amber
-              ),
-              ),
-
-            ),
-
-            Text("fourt item",
-            textDirection: TextDirection.rtl,
-            style: TextStyle(color: Colors.blue
-            ),
-            ),
-
-            Container(
-              color: Colors.black54,
-
-              child: Text("fifth item", 
-              textDirection: TextDirection.ltr,
-              style:TextStyle(color: Colors.pink
-              ),
-              ),
-
-            ),
-            Text("six item",
-            textDirection: TextDirection.rtl,
-            style: TextStyle(color: Colors.blue
-            ),
-            ),
+             Text("item 4",textDirection: TextDirection.ltr,
+            style:TextStyle(color:Colors.blue),),
           ],
- 
-
-        ),
-      
-     child: Text("six item",
-            textDirection: TextDirection.rtl,
-            style: TextStyle(color: Colors.blue
-            ),
-            ),
+      ),
+     
         
         ),
 
